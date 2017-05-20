0003fb71bdc3fe2fd2e34823eb02c260f4bfde88
solosito <alfonso.troya@gmail.com>
2017-05-20 12:19:12 +0200

    First commit

---
+++ task2.cpp
@@ -0,0 +1,122 @@
+// STD Headers
+#include <fstream>  // Input/output stream class to operate on files
+#include <sstream>  // Stream class to operate on strings.
+#include <iostream> // Standard Input / Output Streams Library
+#include <string>   // Introduces string types
+#include <vector>   // Introduces vector types
+#include <cmath>    // Used for cmath::lround
+#include <cassert>  // std::assert
+
+using namespace std;
+
+// Function for parsing the input file
+vector<vector<string>> parseCSV (ifstream &ifile){
+
+    // Variables for storaging input file
+    string line;    
+    vector<vector<string>> values;
+
+    // Check that file is OK
+    if(ifile.good()){
+    // Parse and store input file
+        while(getline(ifile, line)){
+
+            string cell;
+            vector<string> line_values;
+            stringstream lineStream(line);
+
+            while(getline(lineStream, cell, ',')){
+
+                line_values.push_back(cell);
+
+            }
+
+            values.emplace_back(line_values);
+
+        }
+    }
+
+    return values;
+}
+
+// Function for fixing the data and write it to a file
+void fixAndSave (const vector<vector<string>> &data, const int &k, ofstream &ofile){
+
+    vector<vector<string>> values = data;
+
+    // Check errors and save to output file
+    for(unsigned int i = 0; i < values.size(); i++){
+        for(unsigned int j = 0; j < values[i].size(); j++){
+
+            // If there is an error, interpolate its value with its k-neighbors
+            if (stoi(values[i][j]) == 0) {
+
+                int interp = 0;
+                int n = 0;
+
+                for(char u = -k; u <= k; u++){
+                    for(char v = -k; v <= k; v++){
+                        if( (u != 0) && (v != 0) && 
+                            (i+u >= 0) && (j+v >= 0) && 
+                            (i+u <= values.size()-1) && (j+v <= values.size()-1)){
+
+                                interp += stoi(values[i+u][j+v]);
+                                n++;
+
+                        }
+                    }
+                }
+
+                values[i][j] = to_string(lround(interp/n));
+
+            }
+            
+            // Save the data in the output file
+            if (j == values.size()-1)
+                ofile << values[i][j] << endl;
+            else
+                ofile << values[i][j] << ",";
+        }
+    }
+    // Close the file
+    ofile.close();
+}
+
+// Function for checking input arguments
+bool checkArgs(const int &argc, char* argv[]){
+
+    // Check number of arguments
+    if (argc != 3){
+        // Print usage info
+        std::cerr << "\n\n\tUsage: " << argv[0] << " <input>" << " <output>\n\n" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+// Main Function
+int main(int argc, char* argv[])
+{
+    // Arguments check
+	assert(checkArgs(argc, argv));
+
+    // Files paths
+    const char* const ipath = argv[1];
+    const char* const opath = argv[2];
+
+    // Input / Output files
+    ifstream ifile(ipath);
+    ofstream ofile(opath);
+    
+    // k neighbors for interpolation
+    uint8_t k = 1;
+
+    // Variable for storaging the parsed input file
+    vector<vector<string>> values = parseCSV(ifile);
+
+    // Check errors, interpolate them and save to file
+    fixAndSave(values, k, ofile);
+
+
+    return 0;
+}
---
+++ task3.cpp
@@ -0,0 +1,66 @@
+// STD Headers
+#include <iostream>   // Input/Output
+#include <list>       // List Container
+#include <vector>     // Vector Container
+#include <algorithm>  // std::sort
+#include <cassert>    // std::assert
+
+using namespace std;
+
+// Struct with overload
+struct comp{
+  template <typename T, typename U>
+  bool operator() (const pair<T, U> & a, const pair<T, U> & b) const{
+    return a.second < b.second;
+  }
+};
+
+// Function for sorting containers
+template<typename T1, typename T2>
+void sortWrtContainer2(T1& container1, const T2& container2)
+{
+
+  // Check containers have the same size
+  assert(container1.size() == container2.size() && "Containers sizes mismatch.");
+
+  // Variable for storaging pairs of values
+  typedef std::pair<typename T1::value_type,typename T2::value_type> TPair;
+
+  // Vector of pairs for storing the data
+  vector<TPair> data(container1.size());
+
+  // Fill the data vector of pairs with the content of the containers
+  for (auto it1 = container1.begin(); it1!=container1.end(); it1++){
+    auto pos = distance(container1.begin(), it1);
+    auto it2 = container2.begin();
+    advance(it2, pos);
+    data[pos] = make_pair(*it1, *it2);
+  }
+
+  // Sort the data vector
+  stable_sort(data.begin(), data.end(), comp());
+
+  // Store sorted values in first container and print the result
+  unsigned int pos = 0;
+  for (const auto& pair : data){
+    pos = &pair-&data[0];
+    auto it = container1.begin();
+    advance(it, pos);
+    (*it) = pair.first;
+    cout << "Pos " << pos << " -> " << (*it) << ", " << pair.second << endl;
+  }
+}
+
+
+// Main function
+int main(){
+
+  // Containers with data
+  vector<char> container1 = {'a','b','c'};
+  vector<float> container2 = {4,7,1};
+
+  // Sort the containers
+  sortWrtContainer2(container1, container2);
+
+  return 0;
+}
